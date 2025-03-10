548c8bd522e9662c280f2f6f117f2827c92c4b38
Adam Harris <adamtjharris@gmail.com>
2025-03-10 12:40:41 +0100

    exercises 2 and 3

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -5,7 +5,7 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
-if (receivedPassword = SUPER_SECRET_PASSWORD){
+if (receivedPassword === SUPER_SECRET_PASSWORD){
     console.log("Welcome! You are logged in as Brunhilde1984.")
 } else {
     console.log("Access denied")
@@ -35,8 +35,27 @@ if (receivedPassword === SUPER_SECRET_PASSWORD) {
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2 === 0) {
+    console.log("even")
+} else {
+    console.log("odd")
+}
+
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
+let hotdogPrice = 2
+
+if (numberOfHotdogs >= 1000000) {
+    hotdogPrice = 0.10
+} else if (numberOfHotdogs >= 100) {
+    hotdogPrice = 1
+} else if (numberOfHotdogs >= 5) {
+    hotdogPrice = 1.50
+} else {
+}
+
+console.log(hotdogPrice)
+console.log(numberOfHotdogs*hotdogPrice)
 
 // Part 4: Daytime
 const currentHour = 12;
