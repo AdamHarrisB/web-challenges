b1aa35c0fd8ad583964ab928a230c3f55f210443
Adam Harris <adamtjharris@gmail.com>
2025-03-10 10:58:14 +0100

    make terminal work

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -5,6 +5,13 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (SUPER_SECRET_PASSWORD = "h4x0r1337"){
+    console.log("Welcome! You are logged in as Brunhilde1984.")
+} else {
+    console.log("Access denied")
+}
+
+
 // Part 2: Even / Odd
 const number = 6;
 
