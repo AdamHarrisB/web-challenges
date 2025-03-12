6e99321b6e0c3a7ab8de54682610b475367c6432
Adam Harris <adamtjharris@gmail.com>
2025-03-12 15:35:19 +0100

    challenges

--- js-inputs-and-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -4,5 +4,7 @@ const input = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 
 uppercaseButton.addEventListener("click", () => {
-  input.value = input.value.toUpperCase();
+  //input.value = input.value.toUpperCase();
+  input.value = input.value.toLowerCase();
+  
 });
