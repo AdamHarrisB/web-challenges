5c6632320f56f66a18f5f707a39f64ec419aaaf1
Adam Harris <adamtjharris@gmail.com>
2025-03-26 10:35:56 +0100

    solve final react challenge

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -1,5 +1,10 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <article>
+    <h2 className = "article_title">This is the article</h2>
+    <label htmlFor="typebox">This is an input </label>
+    <input id="typebox"></input>
+    <a className="article_link" href="https://en.wikipedia.org/wiki/Main_Page">Click here!</a>
+  </article>;
 }
