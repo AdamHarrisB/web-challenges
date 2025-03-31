1a5ee30f8f3fcc8fddbdb071f1caa0a87050f505
Adam Harris <adamtjharris@gmail.com>
2025-03-31 20:16:32 +0200

    changes initially

--- js/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js/js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,12 @@
 console.clear();
+
+const inputElement = 
+document.querySelector('[data-js="first-input"]');
+
+const buttonup = 
+document.querySelector('[data-js="button-uppercase"]');
+
+buttonup.addEventListener("click", () => {
+    userInput.value.toUpperCase();
+    inputElement.value = inputElement.value.toUpperCase();
+});
