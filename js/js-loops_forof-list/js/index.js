07874df72d684ce9ccee4bc13bd8792523f5815a
Adam Harris <adamtjharris@gmail.com>
2025-04-01 19:04:37 +0200

    solution for for of

--- js/js-loops_forof-list/js/index.js
+++ js/js-loops_forof-list/js/index.js
@@ -17,4 +17,16 @@ const programmingLanguages = [
 
 // --v-- write or modify code below this line --v--
 
+for (const programmingLanguage of programmingLanguages) {
+  //this is a test of the for of loop
+  console.log(programmingLanguage);
+  //define a new constant li
+  const li = document.createElement("li");
+  //add text content to the li element
+  li.textContent = programmingLanguage;
+  //append a li to the ordered list element
+  ol.append(li);
+
+}
+
 // --^-- write or modify code above this line --^--
