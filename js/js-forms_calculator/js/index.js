b14683cb901d03fb6c12b24b430ded53b6f356d2
Adam Harris <adamtjharris@gmail.com>
2025-04-01 12:00:56 +0200

    complete final challenge

--- js/js-forms_calculator/js/index.js
+++ js/js-forms_calculator/js/index.js
@@ -21,14 +21,32 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  if 
-  else if
-  else if
-  else if
+
   let result;
 
   // --v-- write your code here --v--
   // making arbitrary changes for the sake of it
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+    const numberA = 
+  Number(data.numberA);
+    const numberB =
+  Number(data.numberB);
+    const operator = data.operator;
+
+  if (operator === "addition") {
+    result = add(numberA, numberB);
+  }
+  if (operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  }
+  if (operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  }
+  if (operator === "division") {
+    result = divide(numberA, numberB);
+  }
 
   // --^-- write your code here --^--
 
