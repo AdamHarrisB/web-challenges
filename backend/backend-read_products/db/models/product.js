970113462178aadbf1665c999bab02515189ad21
Adam Harris <adamtjharris@gmail.com>
2025-04-25 09:27:29 +0200

    update before starting work

---
+++ backend/backend-read_products/db/models/product.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+	name: { type: String, require: true },
+    description: { type: String, require: true},
+    price: { type: Number, require: true },
+    currency: { type: String, require: true },
+});
+
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
